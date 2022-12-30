b0dc442d6ed1217f936f3d7c208e7c1057c76459
faraawaheeda <76742944+faraawaheeda@users.noreply.github.com>
2022-12-30 22:27:56 +0800

    Update interfaceTextSummarization.py

--- interfaceTextSummarization.py
+++ interfaceTextSummarization.py
@@ -17,7 +17,8 @@ choice = st.sidebar.selectbox("Select Activity", activities)
 if choice == 'News Article': 
   category = ["A","B","C","D"]
   option = st.selectbox("Select News Article", category)
-  
+    
+  """
   if category == 'A':
     st.button("Ad sales boost Time Warner profit")
     if st.button("Ad sales boost Time Warner profit")
@@ -25,4 +26,27 @@ if choice == 'News Article':
         for line in urllib.request.urlopen(url): 
             st.write(line.decode('utf-8'))
   
+  """
+if choice == 'Summarize': 
+   st.subheader("EXTRACTIVE TEXT SUMMARIZER")
+   agree = st.checkbox('Show sentence')
+   raw_text = st.text_area("Original Content","Enter text here")
+   uploaded_file = st.file_uploader("Choose a file")
+   if uploaded_file is not None:
+      # To read file as bytes:
+      bytes_data = uploaded_file.getvalue()
+      st.write(bytes_data)
+      # To convert to a string based IO:
+      stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+      st.write(stringio)
+      # To read file as string:
+      string_data = stringio.read()
+      st.write(string_data)
+      # Can be used wherever a "file-like" object is accepted:
+      dataframe = pd.read_csv(uploaded_file)
+      st.write(dataframe)
+   if st.button("Summarize"):
+      st.write(raw_text)
+      st.button("Copy text")
+      st.write("Words:")
   
