19d2c26e4f2da2640e8472ec16f60c4095510008
faraawaheeda <76742944+faraawaheeda@users.noreply.github.com>
2022-12-30 21:34:57 +0800

    Update interfaceTextSummarization.py

--- interfaceTextSummarization.py
+++ interfaceTextSummarization.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.set_page_config(page_title="Extractive Text Summarization", page_icon=":tada:", layout="wide")
-st.markdown("<h1 style='text-align: center; color: white;'>ONLINE ENGLISH FICTION BOOK REVIEWS EXTRACTIVE TEXT SUMMARIZATION SYSTEM VIA MACHINE LEARNING APPROACHES</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;'>EXTRACTIVE BASED TEXT SUMMARIZATION USING SENTIMENT ANALYSIS</h1>", unsafe_allow_html=True)
 st.markdown("<hr size='5' width='100%;'>", unsafe_allow_html=True)
-activities = ["Introduction","Fiction Books","Summarize","Statistic"]
+activities = ["Introduction","News Article","Summarize","Statistic"]
 choice = st.sidebar.selectbox("Select Activity", activities)
