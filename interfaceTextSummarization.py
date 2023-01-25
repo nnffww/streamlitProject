232a54a4149e6fc329117a9348e19b035548e8dd
Farah Waheeda <76742944+faraawaheeda@users.noreply.github.com>
2023-01-25 15:49:22 +0800

    Update interfaceTextSummarization.py

--- interfaceTextSummarization.py
+++ interfaceTextSummarization.py
@@ -38,7 +38,7 @@ if choice == 'News Article':
   if option == 'Business':
     url = 'https://raw.githubusercontent.com/faraawaheeda/streamlitProject/main/train4.csv?token=GHSAT0AAAAAAB44S4MXLYHZ6NN4P5HTTD5QY6QXD4Q'
     df = pd.read_csv(url,encoding="latin-1")
-    st.write(df.head(11))
+    st.write(df.head(10))
     st.download_button("Download",
                       df.to_csv(),
                       file_name = 'train4.csv',
